424461e81505b4bb5e195b7164496c57df89cfac
nfinformatique <nfrey@nfinformatique.ch>
2014-07-04 17:48:30 +0200

    added ability to modify existing simple moves

--- addons/nf_simple_move/static/js/account_move_line_quickadd.extend.b.js
+++ addons/nf_simple_move/static/js/account_move_line_quickadd.extend.b.js
@@ -2,6 +2,7 @@
 
 
 openerp.nf_simple_move = function(instance){
+	
     instance.web.account.reload = function(parent,action){
     	parent.inner_widget.views.tree_account_move_line_quickadd.controller.reload_content();
     	parent.do_action({"type":"ir.actions.act_window_close"});
@@ -20,9 +21,9 @@ openerp.nf_simple_move = function(instance){
 			this.bind_simple_button();
             return tmp;
 		},
-	    open_wizard: function () {
+	    open_wizard: function (move_line_record_id) {
 	    	var self=this;
-	    	self.dataset.context=$.extend(self.dataset.context,{'current_journal':self.current_journal,'current_period':self.current_period})
+//	    	self.dataset.get_context.add({'current_journal':self.current_journal,'current_period':self.current_period})
 	    	var newwindow=undefined;
 	    	if (self.current_journal && self.current_period){
 	    		newwindow=self.do_action({
@@ -32,10 +33,11 @@ openerp.nf_simple_move = function(instance){
 	//                res_id: self.get("value"),
 	                views: [[false, 'form']],
 	                target: 'new',
-	                context: self.dataset.get_context(),
+	                context: self.dataset.get_context().add({'move_line_record_id':move_line_record_id?move_line_record_id:false}),
+	            }).then(function(){
+		            //SET Tabindex attribute to -1 on unuseful links
+		    		$('.oe_form table.wiz_simple_move .oe_form_uri, a.ui-dialog-titlebar-close, select.oe_debug_view').attr("tabindex","-1");
 	            });
-	            //SET Tabindex attribute to -1 on unuseful links
-	    		$('.oe_form table.wiz_simple_move .oe_form_uri, a.ui-dialog-titlebar-close, select.oe_debug_view').attr("tabindex","-1");
 	            return false;
 	    	}
 	    	return true;
@@ -50,5 +52,30 @@ openerp.nf_simple_move = function(instance){
         },
 	    
 	
-	})
+	});
+	
+	instance.web.ListView.List.include(/** @lends instance.web.ListView.List# */{
+        row_clicked: function (event) {
+        	if (this.view.open_wizard){
+	        	var record_id = $(event.currentTarget).data('id');
+	        	this.view.open_wizard(record_id);
+	        	return false;
+        	}else{
+        		return this._super.apply(this, arguments);
+        	}
+        },
+/*        get_row_for: function (record) {
+            var id;
+            var $row = this.$current.children('[data-id=' + record.get('id') + ']');
+            if ($row.length) {
+                return $row;
+            }
+            return null;
+        }
+*/    });
+	
+	
+	
 }
+
+
--- addons/nf_simple_move/wizard/simple_move.py
+++ addons/nf_simple_move/wizard/simple_move.py
@@ -14,7 +14,8 @@ class simple_move(osv.osv_memory):
                 'period_id': fields.many2one("account.period","Period",required=True),
                 'date': fields.date("Date",required=True),
                 'move_id': fields.many2one("account.move","Move"),
-                'account_tax_id':fields.many2one('account.tax', 'Tax'),
+                'debit_tax_id':fields.many2one('account.tax', 'Tax Credit'),
+                'credit_tax_id':fields.many2one('account.tax', 'Tax Debit'),
                 'amount': fields.float('Amount', digits_compute=dp.get_precision('Account')),
                 'currency_id': fields.many2one('res.currency', 'Currency', help="The optional other currency if it is a multi-currency entry."),
                 'credit_account_id': fields.many2one('account.account', 'Credit Account', required=True, ondelete="cascade", domain=[('type','<>','view'), ('type', '<>', 'closed')], select=2),
@@ -24,27 +25,52 @@ class simple_move(osv.osv_memory):
     
     def _get_default_date(self,cr,uid,context={}):
         date=None
-        period_obj=self.pool.get("account.period")
-        if 'journal_id' in context and 'period_id' in context:
-            cr.execute('''SELECT move_id, date FROM account_move_line
-                        WHERE journal_id = %s AND period_id = %s
-                         ORDER BY date DESC limit 1''', (context['journal_id'], context['period_id']))
-            res = cr.fetchone()
-            date = res and res[1] or period_obj.browse(cr, uid, context['period_id'], context=context).date_start
         
+        if 'move_line_record_id' in context:
+            move_line_obj=self.pool.get("account.move.line")
+            move_line = move_line_obj.browse(cr,uid,context['move_line_record_id'])
+            return move_line.move_id.date 
+            #get date from move
+        else:
+            period_obj=self.pool.get("account.period")
+            if 'journal_id' in context and 'period_id' in context:
+                cr.execute('''SELECT move_id, date FROM account_move_line
+                            WHERE journal_id = %s AND period_id = %s
+                             ORDER BY date DESC limit 1''', (context['journal_id'], context['period_id']))
+                res = cr.fetchone()
+                date = res and res[1] or period_obj.browse(cr, uid, context['period_id'], context=context).date_start
+            
         return date
     
     def _get_default_journal(self,cr,uid,context={}):
+        if 'move_line_record_id' in context:
+            move_line_obj=self.pool.get("account.move.line")
+            move_line = move_line_obj.browse(cr,uid,context['move_line_record_id'])
+            return move_line.move_id.journal_id.id 
+            #get date from move
+        
         if 'journal_id' in context:
             return context['journal_id']
         return False
     
     def _get_default_company(self,cr,uid,context={}):
+        if 'move_line_record_id' in context:
+            move_line_obj=self.pool.get("account.move.line")
+            move_line = move_line_obj.browse(cr,uid,context['move_line_record_id'])
+            return move_line.move_id.journal_id.company_id.id 
+            #get date from move
+        
         if 'journal_id' in context:
             return  self.pool.get("account.journal").browse(cr,uid,context['journal_id'],context=context).company_id.id
         return False
     
     def _get_default_period(self,cr,uid,context={}):
+        if 'move_line_record_id' in context:
+            move_line_obj=self.pool.get("account.move.line")
+            move_line = move_line_obj.browse(cr,uid,context['move_line_record_id'])
+            return move_line.move_id.period_id.id
+            #get date from move
+        
         if 'period_id' in context:
             return context['period_id']
         return False
@@ -53,17 +79,123 @@ class simple_move(osv.osv_memory):
         return False
     def _get_default_debit_account(self,cr,uid,context={}):
         return False
+    def _get_default_amount(self,cr,uid,context={}):
+        if 'move_line_record_id' in context:
+            move_line_obj=self.pool.get("account.move.line")
+            move_line = move_line_obj.browse(cr,uid,context['move_line_record_id'])
+            return 0.0
+#            return move_line.move_id.date 
+            #get date from move
+            
+    def _get_default_move(self,cr,uid,context={}):
+        if 'move_line_record_id' in context:
+            move_line_obj=self.pool.get("account.move.line")
+            move_line = move_line_obj.browse(cr,uid,context['move_line_record_id'])
+            return move_line.move_id.id 
+            #get date from move
+        
+    def _get_default_debit_tax(self,cr,uid,context={}):
+        return False
+    def _get_default_credit_tax(self,cr,uid,context={}):
+        return False
+    def _get_default_name(self,cr,uid,context={}):
+        return False
+    def _get_default_alert(self,cr,uid,context={}):
+        osv.except_osv(_('Do not use default fct') ,"Bad guy !")
+        return False
     
     _defaults = {
-        'date': _get_default_date,
-        'journal_id': _get_default_journal,
-        'period_id': _get_default_period,
-        'credit_account_id' : _get_default_credit_account,
-        'debit_account_id' : _get_default_debit_account,
-        'amount': 0.0,
-        'company_id': _get_default_company,
+        'name': _get_default_alert,
+        'date': _get_default_alert,
+        'journal_id': _get_default_alert,
+        'period_id': _get_default_alert,
+        'credit_account_id' : _get_default_alert,
+        'debit_account_id' : _get_default_alert,
+        'company_id': _get_default_alert,
+        'move_id': _get_default_alert,
+        'debit_tax_id':_get_default_alert,
+        'credit_tax_id':_get_default_alert,
+        'amount': _get_default_alert,
+        'currency_id': _get_default_alert,
     }
 
+    def default_get(self,cr,uid,fields,context={}):
+        res={}
+        
+        #Opening existing entry
+        if 'move_line_record_id' in context and context['move_line_record_id']:
+            move_line_obj=self.pool.get("account.move.line")
+            move_line = move_line_obj.browse(cr,uid,context['move_line_record_id'])
+            move=move_line.move_id
+            #get first untaxed line
+            untaxed_line=False
+            taxed_line=False
+            if len(move.line_id)>3:
+                osv.except_osv(_('This entry is not simple enough for me') ,"This entry is not simple enough for me!")
+            for line in move.line_id:
+                if not line.account_tax_id and not line.tax_code_id:
+                    untaxed_line=line
+                if line.account_tax_id and line.tax_code_id:
+                    taxed_line=line
+                    
+            first_line=untaxed_line   
+            second_line=taxed_line
+            if not second_line:
+                for line in move.line_id:
+                    if not line.account_tax_id and not line.tax_code_id and not line == untaxed_line:
+                        second_line=line
+                        
+                
+            res['name']=first_line.name
+            res['date']=move.date
+            res['journal_id']=move.journal_id.id
+            res['period_id']=move.period_id.id
+            res['credit_account_id']=second_line.account_id.id if second_line.credit>0.0 else first_line.account_id.id  
+            res['debit_account_id']=second_line.account_id.id if second_line.debit>0.0 else first_line.account_id.id
+            res['company_id']=move.company_id.id
+            res['move_id']=move.id
+            res['debit_tax_id']=taxed_line.account_tax_id.id if taxed_line and taxed_line.debit>0.0 else False
+            res['credit_tax_id']=taxed_line.account_tax_id.id if taxed_line and taxed_line.credit>0.0 else False
+            res['amount']=first_line.credit if first_line.credit>0.0 else first_line.debit 
+            res['currency_id']=first_line.currency_id.id
+            
+        #Creating new entry
+        else:
+            res['name']=""
+            period_obj=self.pool.get("account.period")
+            if 'journal_id' in context and 'period_id' in context:
+                cr.execute('''SELECT move_id, date FROM account_move_line
+                            WHERE journal_id = %s AND period_id = %s
+                             ORDER BY date DESC limit 1''', (context['journal_id'], context['period_id']))
+                crres = cr.fetchone()
+                res['date'] = crres[1] if crres else period_obj.browse(cr, uid, context['period_id'], context=context).date_start
+            else:
+                res['date']=False
+                                
+            if 'journal_id' in context:
+                res['journal_id']=context['journal_id']
+            else:
+                res['journal_id']=False
+            if 'period_id' in context:
+                res['period_id']= context['period_id']
+            else:
+                res['period_id']=False
+            res['credit_account_id']=False
+            res['debit_account_id']=False
+            if 'journal_id' in context:
+                res['company_id']=self.pool.get("account.journal").browse(cr,uid,context['journal_id'],context=context).company_id.id
+            else:
+                res['company_id']=False
+            
+            res['move_id']=False
+            res['debit_tax_id']=False
+            res['credit_tax_id']=False
+            res['amount']=0.0
+            res['currency_id']=False
+            
+            
+        return res
+        
 
     def get_period_from_date(self,cr,uid,ids,date,period_id,context={}):
         period_obj=self.pool.get("account.period")
@@ -81,7 +213,7 @@ class simple_move(osv.osv_memory):
             if move:
                 for line in move.line_id:
                     modified_ids.append(line.id)
-                    line.unlink(cr,uid)
+                    line.unlink(context=context)
             vals={
                   'name': wiz.name,
                   'debit':wiz.amount,
@@ -91,6 +223,7 @@ class simple_move(osv.osv_memory):
                   'journal_id':wiz.journal_id.id,
                   'period_id':wiz.period_id.id,
                   'date':wiz.date,
+                  'account_tax_id':wiz.debit_tax_id.id,
                   }
             id=move_line_obj.create(cr,uid,vals,context=context)
             if not move:
@@ -106,7 +239,7 @@ class simple_move(osv.osv_memory):
                   'journal_id':wiz.journal_id.id,
                   'period_id':wiz.period_id.id,
                   'date':wiz.date,
-                  'account_tax_id':wiz.account_tax_id.id,
+                  'account_tax_id':wiz.credit_tax_id.id,
                   }
             id=move_line_obj.create(cr,uid,vals,context=context)
             modified_ids.append(map(lambda x:x.id,move.line_id))
@@ -123,19 +256,25 @@ class simple_move(osv.osv_memory):
         return { 'type': 'ir.actions.client', 'tag': 'simple_move.reload', 'params':{'modified_ids':modified_ids,'close':False} }
 #        return { 'type': 'ir.actions.client', 'tag': 'reload' }
     
+#     def action_cancel(self,cr,uid,ids,context={}):
+#         return { 'type': 'ir.actions.client', 'tag': 'simple_move.reload', 'params':{'modified_ids':[],'close':True} }
+    
     def on_change_date(self,cr,uid,ids,date,context={}):
         period_obj=self.pool.get("account.period")
 
         return {}
-    def on_change_credit_account(self,cr,uid,ids,credit_account_id,account_tax_id,context={}):
-        if not account_tax_id:
-            move_line_obj=self.pool.get("account.move.line")
-            return move_line_obj.onchange_account_id(cr,uid,ids,credit_account_id,context=context)
+    def on_change_credit_account(self,cr,uid,ids,credit_account_id,debit_tax_id,credit_tax_id,context={}):
+        move_line_obj=self.pool.get("account.move.line")
+        res=move_line_obj.onchange_account_id(cr,uid,ids,credit_account_id,context=context)
+        if 'value' in res and 'account_tax_id' in res['value'] and res['value']['account_tax_id']:
+            return {'value':{'credit_tax_id':res['value']['account_tax_id'],'debit_tax_id':False}} 
         return {}
-    def on_change_debit_account(self,cr,uid,ids,debit_account_id,account_tax_id,context={}):
-        if not account_tax_id:
-            move_line_obj=self.pool.get("account.move.line")
-            return move_line_obj.onchange_account_id(cr,uid,ids,debit_account_id,context=context)
+    
+    def on_change_debit_account(self,cr,uid,ids,debit_account_id,debit_tax_id,credit_tax_id,context={}):
+        move_line_obj=self.pool.get("account.move.line")
+        res=move_line_obj.onchange_account_id(cr,uid,ids,debit_account_id,context=context)
+        if 'value' in res and 'account_tax_id' in res['value'] and res['value']['account_tax_id']:
+            return {'value':{'debit_tax_id':res['value']['account_tax_id'],'credit_tax_id':False}} 
         return {}
         
 simple_move()
