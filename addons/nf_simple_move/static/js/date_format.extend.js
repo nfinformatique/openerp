147e3bd4581cc9804baac1521fed4e899daf813d
nfinformatique <nfrey@nfinformatique.ch>
2014-07-11 18:28:19 +0200

    misc tests

---
+++ addons/nf_simple_move/static/js/date_format.extend.js
@@ -0,0 +1,10 @@
+openerp.nf_simple_move.date_extend = function(instance){
+	
+/*	var old_fct=instance.web.parse_value;
+	instance.web.parse_value = function (value, descriptor, value_if_empty) {
+		return old_fct(value,descriptor,value_if_empty);
+	}
+*/
+	
+	
+}
